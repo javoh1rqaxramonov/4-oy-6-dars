1f50d3f31b4e0b2ec1d90e82d9c0e0406e847e83
javoh1rqaxramonov <ubotirov22@.com>
2024-05-29 10:58:15 +0500

    finished

---
+++ index.js
@@ -0,0 +1,24 @@
+//1-masala
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 !== 0) {
+//       console.log(i + " ning kvadrati: " + i * i);
+//     }
+//   }
+
+// 2-masala
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * 3);
+// }
+
+
+// 4- masala
+// let yigindi = 0;
+
+// for (let i = 1; i <= 30; i++) {
+//     yigindi += i * i;
+// }
+
+// console.log("1 dan 30 gacha bo'lgan sonlarning kvadratlari yig'indisi: " + yigindi);
+
+
+

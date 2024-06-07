95e7c2b7f86cac0358947838dc865610374ba14a
javoh1rqaxramonov <ubotirov22@.com>
2024-06-07 16:39:54 +0500

    finished

--- index.js
+++ index.js
@@ -1,24 +1,77 @@
-//1-masala
+// 1 - masala
+
 // for (let i = 1; i <= 50; i++) {
-//     if (i % 2 !== 0) {
-//       console.log(i + " ning kvadrati: " + i * i);
+//   if (i % 2 !== 0) {
+//     console.log(i * i);
+//   }
+// }
+
+// 2 - masala
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i * i * i);
+// }
+
+// 3 - masala
+
+// for (let i = 1; i <= 20; i++) {
+//   let factorial = 1;
+//   for (let j = 1; j <= i; j++) {
+//     factorial *= j;
+//   }
+//   console.log(factorial);
+// }
+
+// 4 - masala
+
+// let sum = 0;
+// for (let i = 1; i <= 30; i++) {
+//   sum += i * i;
+// }
+// console.log(sum);
+
+// 5 - masala
+
+// for (let i = 2; i <= 20; i++) {
+//   let isPrime = true;
+//   for (let j = 2; j < i; j++) {
+//     if (i % j === 0) {
+//       isPrime = false;
+//       break;
 //     }
 //   }
-
-// 2-masala
-// for (let i = 1; i <= 10; i++) {
-//     console.log(i * 3);
+//   if (isPrime) {
+//     console.log(i);
+//   }
 // }
 
+// 6 - masala
 
-// 4- masala
-// let yigindi = 0;
-
-// for (let i = 1; i <= 30; i++) {
-//     yigindi += i * i;
+// for (let i = 1; i <= 100; i++) {
+//   if (i * 2 < 100) {
+//     console.log(i);
+//   }
 // }
 
-// console.log("1 dan 30 gacha bo'lgan sonlarning kvadratlari yig'indisi: " + yigindi);
+// 7 -masala
 
+// let n = 100;
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 7 == 0 && i%5 !== 0) {
+//         console.log(i);
+//     }
+// }
 
+// 8 - masala
 
+// let a = +prompt("a sonni kiriting;");
+
+// let sum = 0 ;
+
+// for(let i = 1 ; i<=a ; i++){
+// if(a % i == 0 ){
+// console.log(i);
+// sum+=i ;
+// }
+// }
+// console.log(sum);
